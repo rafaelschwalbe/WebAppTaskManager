0463ed6ef48ecccbdad012ebfb998634163c8da4
Rafael Pacheco <rafael.schwalbe@outlook.com>
2023-08-29 15:21:52 -0300

    Criado modelo de dados "Task"

---
+++ Domain/Entities/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppTaskManager.Domain.Entities
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Status { get; set; }
+    }
+}
